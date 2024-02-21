3f95ddb567be340d32f1a6febd51b7e522528685
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-02-22 00:52:29 +0100

    fix scss files

--- js/handlers/search.mjs
+++ js/handlers/search.mjs
@@ -11,12 +11,18 @@ export function searchInput() {
     if(value.length > 0) {
       const posts = await searchPosts(value)
 
-      for(let i = 0; i < posts.data.length; i++) {
-        searchResults.innerHTML += `<li class="list-group-item text-wrap bg-light">
-        <a href="/feed/?postId=${posts.data[i].id}" class="text-dark link-underline link-underline-opacity-0"> ${posts.data[i].title}"</a>
-        </li>`
+      if(posts.data.length > 0) {
+        for(let i = 0; i < posts.data.length; i++) {
+          searchResults.innerHTML += `<li class="list-group-item text-wrap bg-light">
+          <a href="/feed/?postId=${posts.data[i].id}" class="text-dark link-underline link-underline-opacity-0"> ${posts.data[i].title}"</a>
+          </li>`
+        }
+        // console.log(posts)
+      }else {
+          searchResults.innerHTML = `<li class="list-group-item text-wrap bg-light">
+          No results
+          </li>`;
       }
-      console.log(posts)
     }
   })
 
